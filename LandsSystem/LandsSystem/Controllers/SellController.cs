527c1873343e6e290ab9504ff43014062d4ef7d1
Nikolay <Filirien@abv.bg>
2017-04-26 21:30:37 +0300

    Fixed Create Post Method and Reworked Html for /Sell/.....
    Button create working
    /Sell/House
    /Sell/Apartment
    /Sell/Land

--- LandsSystem/LandsSystem/Controllers/SellController.cs
+++ LandsSystem/LandsSystem/Controllers/SellController.cs
@@ -4,6 +4,7 @@
     using LandsSystem.Models;
     using Microsoft.AspNet.Identity;
     using System.ComponentModel.DataAnnotations;
+    using System.Net;
     using System.Web.Mvc;
 
     public class SellController : Controller
@@ -16,82 +17,116 @@
 
         [Authorize]
         [HttpGet]
-        public ActionResult Houses()
+        public ActionResult CreateHouse()
         {
             return View();
         }
 
         [Authorize]
         [HttpPost]
-        public ActionResult Houses(HouseCreateModel model)
+        public ActionResult CreateHouse(HouseCreateModel model)
         {
             if (this.ModelState.IsValid)
             {
-                var ownerId = this.User.Identity.GetUserId();
-
-                var house = new House
+                if (model == null)
                 {
-                    Address = model.Address,
-                    Price = model.Price,
-                    YearOfBuilt = model.YearOfBuilt,
-                    LandArea = model.LandArea,
-                    HouseArea = model.HouseArea,
-                    Floors = model.Floors,
-                    Bedrooms = model.Bedrooms,
-                    LivingRooms = model.LivingRooms,
-                    Bathrooms = model.Bathrooms,
-                    HaveBasement = model.HaveBasement,
-                    HavePool = model.HavePool,
-                    HaveGarage = model.HaveGarage,
-                    ParkSlots = model.ParkSlots,
-                    ImageUrl = model.ImageUrl
-                };
-                var db = new LandsDbContext();
-                db.Houses.Add(house);
-                db.SaveChanges();
-                return RedirectToAction("Details", new { id = house.Id });
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                using (var context = new LandsDbContext())
+                {
+                    var ownerId = this.User.Identity.GetUserId();
 
+                    var house = new House
+                    {
+                        Address = model.Address,
+                        Price = model.Price,
+                        YearOfBuilt = model.YearOfBuilt,
+                        LandArea = model.LandArea,
+                        HouseArea = model.HouseArea,
+                        Floors = model.Floors,
+                        Bedrooms = model.Bedrooms,
+                        LivingRooms = model.LivingRooms,
+                        Bathrooms = model.Bathrooms,
+                        HaveBasement = model.HaveBasement,
+                        HavePool = model.HavePool,
+                        HaveGarage = model.HaveGarage,
+                        ParkSlots = model.ParkSlots,
+                        ImageUrl = model.ImageUrl
+                    };
+
+                    context.Houses.Add(house);
+                    context.SaveChanges();
+
+                    var adHouse = new HouseAdvertise
+                    {
+                        Description = model.Description,
+                        SellerId = ownerId,
+                        HouseId = house.Id
+                    };
+
+                    context.HouseAdvertises.Add(adHouse);
+                    context.SaveChanges();
+
+                    return RedirectToAction("Houses","Buy");
+                }
             }
             return View(model);
         }
 
         [Authorize]
         [HttpGet]
-        public ActionResult Apartments()
+        public ActionResult CreateApartment()
         {
             return View();
         }
 
         [Authorize]
         [HttpPost]
-        public ActionResult Apartments(ApartmentCreateModel model)
+        public ActionResult CreateApartment(ApartmentCreateModel model)
         {
             if (this.ModelState.IsValid)
             {
-                var ownerId = this.User.Identity.GetUserId();
-
-                var apartment = new Apartment
+                if (model == null)
                 {
-                    Address = model.Address,
-                    Price = model.Price,
-                    YearOfBuilt = model.YearOfBuilt,
-                    ApartmentArea = model.ApartmentArea,
-                    Floor = model.Floor,
-                    Bedrooms = model.Bedrooms,
-                    LivingRooms = model.LivingRooms,
-                    Bathroom = model.Bathroom,
-                    TerraceArea=model.TerraceArea,
-                    HaveBasement = model.HaveBasement,
-                    HaveElevator = model.HaveElevator,
-                    HaveGarage = model.HaveGarage,
-                    ParkSlots = model.ParkSlots,
-                    ImageUrl = model.ImageUrl
-                };
-                var db = new LandsDbContext();
-                db.Apartments.Add(apartment);
-                db.SaveChanges();
-                return RedirectToAction("Details", new { id = apartment.Id });
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                using (var context = new LandsDbContext())
+                {
 
+                    var ownerId = this.User.Identity.GetUserId();
+
+                    var apartment = new Apartment
+                    {
+                        Address = model.Address,
+                        Price = model.Price,
+                        YearOfBuilt = model.YearOfBuilt,
+                        ApartmentArea = model.ApartmentArea,
+                        Floor = model.Floor,
+                        Bedrooms = model.Bedrooms,
+                        LivingRooms = model.LivingRooms,
+                        Bathroom = model.Bathroom,
+                        TerraceArea = model.TerraceArea,
+                        HaveBasement = model.HaveBasement,
+                        HaveElevator = model.HaveElevator,
+                        HaveGarage = model.HaveGarage,
+                        ParkSlots = model.ParkSlots,
+                        ImageUrl = model.ImageUrl
+                    };
+
+                    context.Apartments.Add(apartment);
+                    context.SaveChanges();
+
+                    var adApartment = new ApartmentAdvertise
+                    {
+                        Description=model.Description,
+                        SellerId=ownerId,
+                        ApartmentId=apartment.Id
+                    };
+
+                    context.ApartmentAdvertises.Add(adApartment);
+                    context.SaveChanges();
+                    return RedirectToAction("Apartments","Buy");
+                }
             }
             return View(model);
         }
@@ -99,34 +134,51 @@
 
         [Authorize]
         [HttpGet]
-        public ActionResult Lands()
+        public ActionResult CreateLand()
         {
             return View();
         }
 
         [Authorize]
         [HttpPost]
-        public ActionResult Lands(LandCreateModel model)
+        public ActionResult CreateLand(LandCreateModel model)
         {
+            if (model==null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
             if (this.ModelState.IsValid)
             {
-                var ownerId = this.User.Identity.GetUserId();
-
-                var land = new Land
+                using (var context = new LandsDbContext())
                 {
-                    Address = model.Address,
-                    Price = model.Price,
-                    Area = model.Area,
-                    Electricity=model.Electricity,
-                    Water=model.Water,
-                    Sewage=model.Sewage,
-                    ImageUrl = model.ImageUrl
-                };
-                var db = new LandsDbContext();
-                db.Lands.Add(land);
-                db.SaveChanges();
+                    var ownerId = this.User.Identity.GetUserId();
 
-                return RedirectToAction("Details", new { id = land.Id });
+                    var land = new Land
+                    {
+                        Address = model.Address,
+                        Price = model.Price,
+                        Area = model.Area,
+                        Electricity = model.Electricity,
+                        Water = model.Water,
+                        Sewage = model.Sewage,
+                        ImageUrl = model.ImageUrl
+                    };
+
+                    context.Lands.Add(land);
+                    context.SaveChanges();
+
+                    var adLand = new LandAdvertise()
+                    {
+                        Description = model.Description,
+                        SellerId = ownerId,
+                        LandId=land.Id
+                    };
+
+                    context.LandAdvertises.Add(adLand);
+                    context.SaveChanges();
+
+                    return RedirectToAction("Lands","Buy");
+                }
             }
             return View(model);
         }
