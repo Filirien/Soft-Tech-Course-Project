1583f3b9fe33e25e2bbd70b013de6993033c9909
MarinKunovski <marin.kunovski@gmail.com>
2017-04-27 20:21:15 +0300

    Fixed Register Bug

--- LandsSystem/LandsSystem/Controllers/AccountController.cs
+++ LandsSystem/LandsSystem/Controllers/AccountController.cs
@@ -8,6 +8,7 @@ using Microsoft.Owin.Security;
 using LandsSystem.Models;
 using LandsSystem.Data;
 using System;
+using System.Data.Entity.Validation;
 
 namespace LandsSystem.Controllers
 {
@@ -21,7 +22,7 @@ namespace LandsSystem.Controllers
         {
         }
 
-        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
+        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
         {
             UserManager = userManager;
             SignInManager = signInManager;
@@ -33,9 +34,9 @@ namespace LandsSystem.Controllers
             {
                 return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
             }
-            private set 
-            { 
-                _signInManager = value; 
+            private set
+            {
+                _signInManager = value;
             }
         }
 
@@ -119,7 +120,7 @@ namespace LandsSystem.Controllers
             // If a user enters incorrect codes for a specified amount of time then the user account 
             // will be locked out for a specified amount of time. 
             // You can configure the account lockout settings in IdentityConfig
-            var result = await SignInManager.TwoFactorSignInAsync(model.Provider, model.Code, isPersistent:  model.RememberMe, rememberBrowser: model.RememberBrowser);
+            var result = await SignInManager.TwoFactorSignInAsync(model.Provider, model.Code, isPersistent: model.RememberMe, rememberBrowser: model.RememberBrowser);
             switch (result)
             {
                 case SignInStatus.Success:
@@ -150,27 +151,41 @@ namespace LandsSystem.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new User {
+                var user = new User
+                {
                     UserName = model.Email,
                     Email = model.Email,
+                    Age = model.Age,
                     FirstName = model.LastName,
                     LastName = model.LastName,
                     PhoneNumber = model.PhoneNumber,
-                    RegisteredOn = DateTime.Now};
-                var result = await UserManager.CreateAsync(user, model.Password);
-                if (result.Succeeded)
-                {
-                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
-                    // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
-                    // Send an email with this link
-                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+                    RegisteredOn = DateTime.Now
+                };
 
-                    return RedirectToAction("Index", "Home");
+                try
+                {
+                    var result = await UserManager.CreateAsync(user, model.Password);
+                    if (result.Succeeded)
+                    {
+                        await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+
+                        // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
+                        // Send an email with this link
+                        // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+                        // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                        // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+
+                        return RedirectToAction("Index", "Home");
+                    }
+                    AddErrors(result);
                 }
-                AddErrors(result);
+                catch (DbEntityValidationException e)
+                {
+
+                    var validationErrors = e.EntityValidationErrors;
+
+                    RedirectToAction("Register");
+                }
             }
 
             // If we got this far, something failed, redisplay form
